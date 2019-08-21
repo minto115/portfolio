0f7eb0aae33a7af25cd85ca3a75537ac015ef0ab
minto115 <nextfire118@gmail.com>
2019-08-21 10:29:42 +0000

    signup-user finish

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def top
   end
+  
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
   root 'static_pages#top'
+  get 'about', to: "static_pages#about"
   get '/signup' , to: "users#new"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
