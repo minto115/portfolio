c83a44a8a355ff5cbd83757bddc96b09f0a9ad4b
minto115 <nextfire118@gmail.com>
2019-09-03 12:04:33 +0000

    update-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :log_in_user, only:[:index, :update, :edit, :destroy]
+  before_action :correct_user, only:[:edit, :update]
+  before_action :admin_user, only:[:index, :destroy]
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def show
@@ -17,10 +24,47 @@ class UsersController < ApplicationController
       render "new"
     end
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "アカウントの編集に成功しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "ユーザーを削除しました。"
+    redirect_to users_url
+  end
+      
   private
 
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+    
+    def log_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください"
+      redirect_to login_path
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
